f6039c0c4b7ad41bca4425179c45f788dc623861
ayan4m1 <andrew@bulletlogic.com>
2023-07-04 00:15:32 -0400

    chore: update react package ref to analyzer

---
+++ packages/react/src/index.ts
@@ -0,0 +1,21 @@
+import { WebAudioCore } from '@webaudio/core';
+import { useEffect, useRef } from 'react';
+
+export function useWebAudio() {
+  const coreRef = useRef<WebAudioCore>(null);
+
+  useEffect(() => {
+    if (coreRef.current) {
+      return;
+    }
+
+    coreRef.current = new WebAudioCore();
+
+    return () => coreRef.current.disconnect();
+  }, []);
+
+  return {
+    getSources: WebAudioCore.getSources,
+    ref: coreRef.current
+  };
+}
