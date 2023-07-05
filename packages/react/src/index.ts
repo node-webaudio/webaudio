5cd1ce1c085990de9a34f630ceb19c503bbacb44
ayan4m1 <andrew@bulletlogic.com>
2023-07-05 00:12:07 -0400

    feat(react): re-organize API to be cleaner

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,22 +1,70 @@
-import { AudioAnalyzer } from '@webaudio/analyzer';
-import { useEffect, useState } from 'react';
+import {
+  AudioAnalyzer,
+  AudioKind,
+  SpectrumEvent,
+  VolumeEvent
+} from '@webaudio/analyzer';
+import { useCallback, useEffect, useState } from 'react';
 
-export function useAudioAnalyzer() {
-  const [analyzer, setAnalyzer] = useState<AudioAnalyzer>(null);
+export function useAudioSources(kind: AudioKind = 'audioinput') {
   const [sources, setSources] = useState<MediaDeviceInfo[]>([]);
 
-  async function updateSources() {
-    setSources(await AudioAnalyzer.getSources());
-  }
+  const refresh = useCallback(
+    async () => setSources(await AudioAnalyzer.getSources(kind)),
+    []
+  );
 
   useEffect(() => {
-    setAnalyzer(new AudioAnalyzer());
-    updateSources();
+    refresh();
   }, []);
 
   return {
-    analyzer,
     sources,
-    updateSources
+    refresh
   };
+}
+
+export function useAudioAnalyzer() {
+  const [analyzer, setAnalyzer] = useState<AudioAnalyzer>(null);
+
+  useEffect(() => {
+    const newAnalyzer = new AudioAnalyzer();
+    setAnalyzer(newAnalyzer);
+  }, []);
+
+  return analyzer;
+}
+
+export function useVolume(
+  analyzer: AudioAnalyzer,
+  handler: (event: VolumeEvent) => void
+) {
+  useEffect(() => {
+    if (!analyzer) {
+      return;
+    }
+
+    analyzer.on('volume', handler);
+
+    return () => {
+      analyzer.off('volume', handler);
+    };
+  }, [analyzer]);
+}
+
+export function useSpectrum(
+  analyzer: AudioAnalyzer,
+  handler: (event: SpectrumEvent) => void
+) {
+  useEffect(() => {
+    if (!analyzer) {
+      return;
+    }
+
+    analyzer.on('spectrum', handler);
+
+    return () => {
+      analyzer.off('spectrum', handler);
+    };
+  }, [analyzer]);
 }
